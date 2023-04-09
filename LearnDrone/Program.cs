43f764ec176f20f3d6cb9e5bb3bddc6c676dc86d
mindtnv <47498401+mindtnv@users.noreply.github.com>
2023-04-09 15:03:56 +0300

    Update .drone.yml

--- LearnDrone/Program.cs
+++ LearnDrone/Program.cs
@@ -2,6 +2,6 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Configuration.AddEnvironmentVariables();
 var app = builder.Build();
 
-app.MapGet("/", (IConfiguration configuration) => $"Hello World! Your secret: {configuration["SECRET_VARIABLE"]}");
+app.MapGet("/", (IConfiguration configuration) => $"Hello World! 123456");
 
 app.Run();
